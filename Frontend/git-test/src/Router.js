59c79ae2008d82466340d16388f17f1ae1834716
mac <mac@macui-MacBookAir.local>
2023-04-27 17:17:00 +0900

    Add: 메인 기능 구현 완료

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-//import App from './App';
-//import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 
 export default function Router() {
